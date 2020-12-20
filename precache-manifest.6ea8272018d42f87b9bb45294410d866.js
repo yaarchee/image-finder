c9925778aa7ce9eac6cf30923ca8906b2eab7681
Yaroslav Paltsun <trancemanjake@gmail.com>
2020-12-20 19:33:16 +0200

    Updates

---
+++ precache-manifest.6ea8272018d42f87b9bb45294410d866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8980c1ea9f92ee77d30983e9fa04eb3",
+    "url": "/image-finder/index.html"
+  },
+  {
+    "revision": "4135a9333a3797cbfe80",
+    "url": "/image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "5532ee06c7046c983f34",
+    "url": "/image-finder/static/css/main.4085b2b4.chunk.css"
+  },
+  {
+    "revision": "4135a9333a3797cbfe80",
+    "url": "/image-finder/static/js/2.15c6445f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/image-finder/static/js/2.15c6445f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5532ee06c7046c983f34",
+    "url": "/image-finder/static/js/main.6e15d71c.chunk.js"
+  },
+  {
+    "revision": "474a11e36546e6d64183",
+    "url": "/image-finder/static/js/runtime-main.8454cb03.js"
+  }
+]);
